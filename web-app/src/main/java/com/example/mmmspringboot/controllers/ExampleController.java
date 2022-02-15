a0659fcb035f01dfbdc0afa16e2d74690cb85390
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-15 13:37:24 +0100

    Final commit

--- web-app/src/main/java/com/example/mmmspringboot/controllers/ExampleController.java
+++ web-app/src/main/java/com/example/mmmspringboot/controllers/ExampleController.java
@@ -1,10 +1,13 @@
 package com.example.mmmspringboot.controllers;
 
 import com.example.mmmspringboot.model.ShippingAddress;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
+
+    @RequestMapping("/")
     public ShippingAddress getShippingAddress(){
         return new ShippingAddress();
     }

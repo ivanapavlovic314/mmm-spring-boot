f742ba738fbe38c693f1c4529cb4d8bc918c2ea1
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-15 13:03:50 +0100

    initial commit

---
+++ src/main/java/com/example/mmmspringboot/MmmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmmSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmmSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mmmspringboot/MmmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmmspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmmSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
